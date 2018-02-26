8cb7ae7d14430421a4cd22203b6c4d2ea8a171e7
zhengchenglei <zhengchenglei@jd.com>
2018-02-26 22:55:29 +0800

    fixed

---
+++ src/main/java/com/firefly/wcreate/conf/SystemProperties.java
@@ -0,0 +1,58 @@
+package com.firefly.wcreate.conf;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * firefly system parameter configuration...
+ * 
+ * @author zhengchenglei
+ *
+ */
+public class SystemProperties {
+	
+	
+	private final static Map<String,Object> setting = new ConcurrentHashMap<String, Object>();
+	
+	
+	
+	public static void put(String k,Object v){
+		setting.put(k, v);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T get(String k,Class<T> clazz){
+		
+		return (T)setting.get(k);
+	}
+	
+	
+	public static enum resource{
+		
+		PORT("port","The port of application"),
+		VIRDIR("virDir","the directory of virtual application"),
+		APPDIR("appDir","the directory of application"),
+		AJP("AJP","AJP protocol"),
+		HTTP("HTTP","HTTP protocol");
+		
+		private String value;
+		
+		private String comment;
+		
+		resource(String value,String comment){
+			this.value = value;
+			this.comment = comment;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public String getComment() {
+			return comment;
+		}
+		
+		
+	}
+
+}
